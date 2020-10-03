90e6ff6e193decece03f1a123f4b57799d2b6811
Henrijs <11@gmail.com>
2020-10-04 01:55:00 +0300

    ok

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import pynput, time, random
 from pynput.keyboard import Key, Controller as KeyboardController, Listener
 from pynput.mouse import Button, Controller as MouseController
